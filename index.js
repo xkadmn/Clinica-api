bdbb2b6ea973d6eaab0a3c9cb885963eeb5efd59
admin <xkblnk@gmail.com>
2025-07-31 01:34:23 -0300

    mejoras veriftoken

--- index.js
+++ index.js
@@ -14,6 +14,22 @@ app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
+ 
+function verificarToken(req, res, next) {
+  const header = req.headers['authorization'];
+  if (!header) {
+    return res.status(401).json({ mensaje: 'Token no proporcionado' });
+  }
+  const token = header.split(' ')[1];
+  jwt.verify(token, SECRET, (err, payload) => {
+    if (err) {
+      return res.status(403).json({ mensaje: 'Token inválido o expirado' });
+    }
+    req.usuario = payload;
+    next();
+  });
+}
+
 // Rutas públicas
 app.get('/prueba', (req, res) => res.send('Hola mundo'));
 app.get('/pruebajson', (req, res) => res.json({ mensaje: 'Hola pruebajson' }));
